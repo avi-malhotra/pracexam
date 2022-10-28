31723d24529b1f89dfaa93a296d1c32fc0157b07
Avi <avimalhotra050703@gmail.com>
2022-10-28 15:43:47 +1030

    commit-1

--- Car.cpp
+++ Car.cpp
@@ -7,14 +7,14 @@ using namespace std;
 
 Car::Car(){
     price=0;
-    VinNumber=0;
+    vinNumber=0;
     emissions=0;
 }
 void Car::set_price(int p){
     price=p;
 }
-void Car::set_VinNumber(int v){
-    VinNumber=v;
+void Car::set_vinNumber(int v){
+    vinNumber=v;
 }
 void Car::set_emissions(int e){
     emissions=e;
@@ -22,8 +22,8 @@ void Car::set_emissions(int e){
 int Car::get_price(){
     return price;
 }
-int Car::get_VinNumber(){
-    return VinNumber;
+int Car::get_vinNumber(){
+    return vinNumber;
 }
 int Car::get_emissions(){
     return emissions;
--- Car.h
+++ Car.h
@@ -9,15 +9,15 @@ using namespace std;
 class Car{
     protected:
     int price;
-    int VinNumber;
+    int vinNumber;
     int emissions=0;
     public:
     Car();
     void set_price(int);
-    void set_VinNumber(int);
+    void set_vinNumber(int);
     void set_emissions(int);
     int get_price();
-    int get_VinNumber();
+    int get_vinNumber();
     int get_emissions();
     virtual void drive(int kms)=0;
 };
